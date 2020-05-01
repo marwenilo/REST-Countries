c47eba57bacc9d97df7294134a4b5000931d7007
Marwen Magri <marwen.magri@gomycode.co>
2020-05-01 03:34:31 +0000

    add search

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
-const useStyles = makeStyles({
+import TextField from "@material-ui/core/TextField";
+
+const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
     display: "flex",
@@ -29,7 +31,13 @@ const useStyles = makeStyles({
     flexWrap: "wrap",
     justifyContent: "space-between",
   },
-});
+  search: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "79vw",
+    },
+  },
+}));
 const flag = (e) => {
   e.toLowerCase();
   return `https://www.countryflags.io/${e}/flat/64.png`;
@@ -37,40 +45,63 @@ const flag = (e) => {
 function App() {
   const [myData, setData] = useState([]);
   const [isCancelled, setIsCancelled] = useState(true);
+  const [search, setSearch] = useState("");
   useEffect(() => {
     isCancelled && AllCountries(myData, setData);
     console.log(myData);
     setIsCancelled(false);
   }, [myData, isCancelled]);
   const classes = useStyles();
-
+  const filteredSearch = myData
+    .sort((a, b) => b - a)
+    .filter((item) => {
+      return item.name.toLocaleLowerCase().includes(search.toLocaleLowerCase());
+    });
+  console.log(filteredSearch, "eee");
   return (
-    <Container fixed className={classes.container}>
-      <CssBaseline />
-      {myData &&
-        myData.map((el, key) => (
-          <Card className={classes.root}>
-            <CardActionArea>
-              <CardContent>
-                <CardContent className={classes.myClass}>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {el.name}
+    <Container fixed>
+      <form className={classes.search} noValidate autoComplete="off">
+        <TextField
+          id="outlined-basic"
+          label="Search"
+          variant="outlined"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
+      <Container fixed className={classes.container}>
+        <CssBaseline />
+        {myData &&
+          filteredSearch.map((el, key) => (
+            <Card className={classes.root} key={key}>
+              <CardActionArea>
+                <CardContent>
+                  <CardContent className={classes.myClass}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {el.name}
+                    </Typography>
+                    <CardMedia
+                      className={classes.media}
+                      image={flag(el.alpha2Code)}
+                      title={el.name}
+                    />
+                  </CardContent>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    The native name of this country is{" "}
+                    <strong>{el.nativeName}</strong> and her capital is{" "}
+                    <strong>{el.capital}</strong> located in {el.region} exactly
+                    at
+                    <strong> {el.subregion}</strong> and thier currencies is{" "}
+                    <strong>{el.currencies}</strong>
                   </Typography>
-                  <CardMedia
-                    className={classes.media}
-                    image={flag(el.alpha2Code)}
-                    title={el.name}
-                  />
                 </CardContent>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  The native name of this country is <strong>{el.nativeName}</strong> and her
-                  capital is <strong>{el.capital}</strong> located in {el.region} exactly at
-                  <strong> {el.subregion}</strong> and thier currencies is <strong>{el.currencies}</strong>
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        ))}
+              </CardActionArea>
+            </Card>
+          ))}
+      </Container>
     </Container>
   );
 }
